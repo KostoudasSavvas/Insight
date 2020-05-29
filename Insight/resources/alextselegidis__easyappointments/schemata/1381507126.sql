4abd74890cea61f633c72f1b5eac3452f5063151
savaf <savaf@DESKTOP-8FA01QU>
2020-05-29 09:58:27 +0300

    Added Resources Schema Files

---
+++ Insight/resources/alextselegidis__easyappointments/schemata/1381507126.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Φιλοξενητής: localhost
+-- Χ�?όνος δημιου�?γίας: 11 Οκτ 2013 στις 16:58:08
+-- Έκδοση διακομιστή: 5.5.24-log
+-- Έκδοση PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+/*!40101 SET NAMES utf8 */
+
+--
+-- Βάση: `easy_appointments`
+--
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_appointments`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_appointments` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `book_datetime` datetime DEFAULT NULL,
+  `start_datetime` datetime DEFAULT NULL,
+  `end_datetime` datetime DEFAULT NULL,
+  `notes` text,
+  `hash` text,
+  `is_unavailable` tinyint(4) DEFAULT '0',
+  `id_users_provider` bigint(20) unsigned DEFAULT NULL,
+  `id_users_customer` bigint(20) unsigned DEFAULT NULL,
+  `id_services` bigint(20) unsigned DEFAULT NULL,
+  `id_google_calendar` text,
+  PRIMARY KEY (`id`),
+  KEY `id_users_customer` (`id_users_customer`),
+  KEY `id_services` (`id_services`),
+  KEY `id_users_provider` (`id_users_provider`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=63 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_roles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `slug` varchar(256) DEFAULT NULL,
+  `is_admin` tinyint(4) DEFAULT NULL COMMENT '0',
+  `appointments` int(4) DEFAULT NULL COMMENT '0',
+  `customers` int(4) DEFAULT NULL COMMENT '0',
+  `services` int(4) DEFAULT NULL COMMENT '0',
+  `users` int(4) DEFAULT NULL COMMENT '0',
+  `system_settings` int(4) DEFAULT NULL COMMENT '0',
+  `user_settings` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_secretaries_providers`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_secretaries_providers` (
+  `id_users_secretary` bigint(20) unsigned NOT NULL,
+  `id_users_provider` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id_users_secretary`,`id_users_provider`),
+  KEY `fk_ea_secretaries_providers_1` (`id_users_secretary`),
+  KEY `fk_ea_secretaries_providers_2` (`id_users_provider`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_services`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_services` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `currency` varchar(32) DEFAULT NULL,
+  `description` text,
+  `id_service_categories` bigint(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_service_categories` (`id_service_categories`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_services_providers`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_services_providers` (
+  `id_users` bigint(20) unsigned NOT NULL,
+  `id_services` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id_users`,`id_services`),
+  KEY `id_services` (`id_services`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_service_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_service_categories` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_settings`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_settings` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) DEFAULT NULL,
+  `value` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_users`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(256) DEFAULT NULL,
+  `last_name` varchar(512) DEFAULT NULL,
+  `email` varchar(512) DEFAULT NULL,
+  `mobile_number` varchar(128) DEFAULT NULL,
+  `phone_number` varchar(128) DEFAULT NULL,
+  `address` varchar(256) DEFAULT NULL,
+  `city` varchar(256) DEFAULT NULL,
+  `state` varchar(128) DEFAULT NULL,
+  `zip_code` varchar(64) DEFAULT NULL,
+  `notes` text,
+  `id_roles` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_roles` (`id_roles`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=84 ;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `ea_user_settings`
+--
+
+CREATE TABLE IF NOT EXISTS `ea_user_settings` (
+  `id_users` bigint(20) unsigned NOT NULL,
+  `username` varchar(256) DEFAULT NULL,
+  `password` varchar(512) DEFAULT NULL,
+  `salt` varchar(512) DEFAULT NULL,
+  `working_plan` text,
+  `notifications` tinyint(4) DEFAULT '0',
+  `google_sync` tinyint(4) DEFAULT '0',
+  `google_token` text,
+  `sync_past_days` int(11) DEFAULT '5',
+  `sync_future_days` int(11) DEFAULT '5',
+  PRIMARY KEY (`id_users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
